976291758744a07135934d0cb1f872a874a4105b
choral7451 <chorales@naver.com>
2022-04-12 19:34:30 +0900

    게시판 페이징 오류 해결

--- app/src/public/js/recruit_religion.js
+++ app/src/public/js/recruit_religion.js
@@ -1,6 +1,8 @@
 const pageButton = () => {
     const URLSearch = new URLSearchParams(location.search);
+    const path = location.pathname;
     const pageNum = document.getElementById('pageNum').firstElementChild.getAttribute('id')
+    const count = document.getElementById('pageNum').childElementCount;
     let getQueryString;
 
     document.getElementById('left').addEventListener('click', ()=> {
@@ -10,18 +12,22 @@ const pageButton = () => {
             getQueryString = URLSearch.get('id')-1;
         }
 
-        document.getElementById('left').setAttribute('href', `http://localhost:3000/recruit_religion/all?id=${getQueryString}`);
+        document.getElementById('left').setAttribute('href', `http://localhost:3000${path}?id=${getQueryString}`);
     });        
 
     document.getElementById('right').addEventListener('click', ()=> {
         if(URLSearch == 0) {
-            getQueryString = "2";
+            if(count == 1) {
+                getQueryString = "1";
+            } else {
+                getQueryString = "2";
+            } 
         } else if (Number(URLSearch.get('id')) >= pageNum) {    
             getQueryString = pageNum
         } else {
             getQueryString = Number(URLSearch.get('id'))+1;
         }
-        document.getElementById('right').setAttribute('href', `http://localhost:3000/recruit_religion/all?id=${getQueryString}`);
+        document.getElementById('right').setAttribute('href', `http://localhost:3000${path}?id=${getQueryString}`);
     })
 
     if(URLSearch.get('id') == null) {
