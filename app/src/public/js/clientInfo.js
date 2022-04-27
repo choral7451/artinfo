f2e7bd6360ba94cdbc8e84c3838f8d188469d431
choral7451 <chorales@naver.com>
2022-04-27 18:24:49 +0900

    고객 정보 모바일 화면 css 수정

--- app/src/public/js/clientInfo.js
+++ app/src/public/js/clientInfo.js
@@ -1,6 +1,6 @@
 const clickMenu = () => {
     if(document.getElementById('mobileMenu').style.display == "flex") {
-        document.getElementById('mobileMenu').firstChild.style.display="none";
+        document.getElementById('mobileMenu').style.display="none";
     } else {
         document.getElementById('mobileMenu').style.display="flex";
     }   
